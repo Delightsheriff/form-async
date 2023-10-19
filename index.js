512a173295fd3159694cfff081debd110645bd32
Delight Sheriff <delightsheriff@gmail.com>
2023-10-19 23:42:37 +0100

    darkmode update

--- index.js
+++ index.js
@@ -57,55 +57,41 @@ form.addEventListener("submit", handleForm);
 
 ///Darkmode
 // check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem("darkMode");
-
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
-
 const darkModeIcon = document.getElementById("dark-mode-icon");
 
+let isDarkMode = false; // Track the current state of dark mode
+
 const enableDarkMode = () => {
-  // 1. Add the class to the body
-  document.body.classList.add("darkmode");
-  // 2. Update darkMode in localStorage
-  localStorage.setItem("darkMode", "enabled");
+  document.body.classList.add("dark-mode");
+  isDarkMode = true;
 };
 
 const disableDarkMode = () => {
-  // 1. Remove the class from the body
-  document.body.classList.remove("darkmode");
-  // 2. Update darkMode in localStorage
-  localStorage.setItem("darkMode", null);
+  document.body.classList.remove("dark-mode");
+  isDarkMode = false;
 };
-
-// If the user already visited and enabled darkMode
-// start things off with it on
-if (darkMode === "enabled") {
-  enableDarkMode();
-}
 
 // When someone clicks the button
 darkModeToggle.addEventListener("click", () => {
-  // get their darkMode setting
-  darkMode = localStorage.getItem("darkMode");
-
-  // if it not current enabled, enable it
-  if (darkMode !== "enabled") {
-    enableDarkMode();
-    // if it has been enabled, turn it off
-  } else {
+  // Toggle dark mode
+  if (isDarkMode) {
     disableDarkMode();
-  }
-});
-
-/////
-darkModeToggle.addEventListener("click", () => {
-  // Toggle the dark mode class on the body element
-  document.body.classList.toggle("dark-mode");
-
-  // Change the image src depending on the dark mode state
-  if (document.body.classList.contains("dark-mode")) {
-    darkModeIcon.src = "dark.svg";
   } else {
+    enableDarkMode();
+  }
+
+  // Toggle the dark mode icon
+  if (isDarkMode) {
     darkModeIcon.src = "light.svg";
+  } else {
+    darkModeIcon.src = "dark.svg";
   }
 });
+
+// Set the initial dark mode state
+if (isDarkMode) {
+  enableDarkMode();
+} else {
+  disableDarkMode();
+}
